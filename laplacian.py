250e1e568be49fbd18835b0330862acce556e366
江春源 <jiangcy1994@users.noreply.github.com>
2019-12-01 23:51:51 +0800

    update laplacian.py

--- laplacian.py
+++ laplacian.py
@@ -10,8 +10,8 @@ def laplacian(input_dir, original_dir, out_dir):
         A = cv2.imread(original_dir + '/' + img_name).astype(np.double);
         C = cv2.imread(input_dir + '/' + img_name).astype(np.double);
 
-        Ads = pyramid_gaussian(A, max_layer=4, multichannel=True)
-        Ls = [Ads[i] - cv2.resize(Ads[i + 1], Ads[i].shape[:2]) for i in range(4)]
-        Cus = accumulate([C] + Ls[::-1], lambda x, y: cv2.resize(x, y.shape[:2]) + y)
+        Ads = list(pyramid_gaussian(A, max_layer=4, multichannel=True))
+        Ls = [Ads[i] - cv2.resize(Ads[i + 1], Ads[i].shape[1::-1]) for i in range(4)]
+        Cus = list(accumulate([C] + Ls[::-1], lambda x, y: cv2.resize(x, y.shape[1::-1]) + y))
 
         cv2.imwrite(out_dir + '/' + img_name, Cus[-1].astype(np.uint8), [cv2.IMWRITE_JPEG_QUALITY, 100])
