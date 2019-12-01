f39c4981fa1af2c525e79a7a1dc3cf290d4c1340
江春源 <jiangcy1994@users.noreply.github.com>
2019-12-01 19:52:31 +0800

    update resize_im.py remove 1 pb file

--- resize_im.py
+++ resize_im.py
@@ -1,4 +1,4 @@
-from cv2 import imread, imresize, imwrite
+from cv2 import imread, imresize, imwrite, IMWRITE_JPEG_QUALITY
 import os
 import sys
 
@@ -7,5 +7,5 @@ def resize_im(input_dir, output_dir):
     
     for imgs_name in os.listdir(input_dir):
         C = imresize(imread(input_dir + '/' + imgs_name), (256, 256))
-        imwrite(C, output_dir + '/' + imgs_name, 'Mode', 'lossless')
+        imwrite(output_dir + '/' + imgs_name, C, [IMWRITE_JPEG_QUALITY, 100])
 
